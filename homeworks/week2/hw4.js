2878e8961c52a275c8309da78f03b56c38f6c4d0
MilyHsieh <hsieh.mily@gmail.com>
2020-07-13 20:50:23 +0800

    week2 finished

--- homeworks/week2/hw4.js
+++ homeworks/week2/hw4.js
@@ -1,5 +1,9 @@
 function printFactor(n) {
-  
+    for (var i = 1; i <= n; i++) {
+        if (i <= n && n % i === 0) {
+            console.log(i)
+        }
+    }
 }
 
 printFactor(10);
--- homeworks/week2/hw5.js
+++ homeworks/week2/hw5.js
@@ -1,9 +1,18 @@
 function join(arr, concatStr) {
-  
+    var result = '';
+    var lastWord = arr.length - 1;
+    for (var i = 0; i < lastWord; i++) {
+        result += arr[i] + concatStr
+    }
+    return result + arr[lastWord]
 }
 
 function repeat(str, times) {
-  
+    var answer = '';
+    for (var i = 1; i <= times; i++) {
+        answer += str
+    }
+    return answer
 }
 
 console.log(join(['a'], '!'));
